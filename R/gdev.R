0ddc57900f2cea995f1e62a52df8cef22599d118
Timothée Giraud <timothee.giraud@cnrs.fr>
2017-03-06 13:06:36 +0000

    version 0.1.0

---
+++ R/gdev.R
@@ -0,0 +1,79 @@
+#' @title General Deviation
+#' @name gdev
+#' @description This function computes the deviation between regional ratios 
+#' and a ratio of reference. 
+#' Each elementary unit's value will be compared to a global value.
+#' @param x a data frame.
+#' @param var1 name of the numerator variable in x.
+#' @param var2 name of the denominator variable in x.
+#' @param ref ratio of reference; if NULL, the ratio of reference is the one of 
+#' the whole study area (\code{sum(var1) / sum(var2)}).
+#' @param type type of deviation; "rel" for relative deviation, "abs" for 
+#' absolute deviation (see Details).
+#' @details 
+#' The relative global deviation is the ratio between var1/var2 and ref
+#' (\code{100 * (var1 / var2) / ref}). Values greater than 100 indicate that the 
+#' unit ratio is greater than the ratio of reference. Values lower than 100 
+#' indicate that the unit ratio is lower than the ratio of reference.\cr
+#' The absolute global deviation is the amount of numerator that could be moved 
+#' to obtain the ratio of reference on all units. 
+#' @return A vector is returned.
+#' @examples
+#' # load data
+#' data("GrandParisMetropole")
+#' # compute absolute global deviation
+#' com$gdevabs <- gdev(x = com, var1 = "INC", var2 = "TH", type = "abs")
+#' # compute relative global deviation
+#' com$gdevrel <- gdev(x = com, var1 = "INC", var2 = "TH", type = "rel")
+#' 
+#' # Deviations maps
+#' if(require('cartography')){
+#'   # set graphical parameters
+#'   par(mar = c(0,0,1.2,0))
+#'   # set breaks
+#'   bks <- c(min(com$gdevrel),50,75,100,125,150,max(com$gdevrel))
+#'   cols <- carto.pal(pal1 = "blue.pal", n1 = 3,
+#'                     pal2 = "wine.pal", n2 = 3)
+#'   # plot a choropleth map of the relative global deviation
+#'   choroLayer(spdf = com.spdf, df = com, var = "gdevrel",
+#'              legend.pos = "topleft",
+#'              legend.title.txt = "Relative Deviation",
+#'              breaks = bks, border = NA,
+#'              col = cols)
+#'   # add symbols proportional to the absolute general deviation
+#'   propSymbolsLayer(spdf = com.spdf, df = com, var = "gdevabs",
+#'                    legend.pos = "left",legend.values.rnd = -2,
+#'                    legend.title.txt = "Absolute Deviation",
+#'                    col = "#ff000050",col2 = "#0000ff50",
+#'                    legend.style = "e", inches = 0.2,
+#'                    breakval = 0)
+#'   # add EPT boundaries
+#'   plot(ept.spdf, add=TRUE)
+#'   # add a layout
+#'   layoutLayer(title = "General Deviation (reference: Grand Paris Metropole)",
+#'               sources = "GEOFLA® 2015 v2.1, Apur, impots.gouv.fr", 
+#'               north = TRUE,
+#'               author = "MTA")
+#' }
+#' @export
+gdev <- function(x, var1, var2, type = "rel", ref = NULL){
+  # test for NAs
+  vtot <- row.names(x)
+  x <- testNAdf(x = x, var1 = var1, var2 = var2)
+  vpar <- row.names(x)
+  
+  # no ref value
+  if (is.null(ref)){
+    ref <- sum(x[,var1]) / sum(x[,var2])
+  }
+  # relative deviation
+  if (type=="rel"){
+    v <- ((x[,var1] / x[,var2]) / ref) * 100
+  }
+  # absolute deviation
+  if (type=="abs"){
+    v <- x[,var1] - (ref * x[,var2])
+  }
+  v <- v[match(vtot, vpar)]
+  return(v)
+}
---
+++ R/package.R
@@ -0,0 +1,106 @@
+#' @title Multiscalar Territorial Analysis
+#' @name MTA
+#' @description 
+#' Build multiscalar territorial analysis based on various contexts. \cr
+#' Main functions : 
+#' \itemize{
+#' \item{\code{\link{gdev}}: general deviation between regional ratios and a 
+#' ratio of reference.}
+#' \item{\code{\link{tdev}}: territorial deviation between regional ratios and 
+#' ratios of an aggregated level.}
+#' \item{\code{\link{sdev}}}: spatial deviation between regional ratios and ratios
+#' of neigborhing regions.
+#' \item{\code{\link{mst}}}: multiscalar typology based on the three deviations.
+#' \item{\code{\link{mas}}}: multiscalar absolute synthesis, total amount of 
+#' redistributions based on the three deviations. 
+#' }
+#' 
+#' @references GRASLAND C., YSEBAERT R., ZANIN C., LAMBERT N., Spatial disparities in Europe  (Chapter 4)  
+#' in GLOERSEN E., DUBOIS A. (coord.), 2007, Regional disparities and cohesion: What Strategies for the future?, DG-IPOL – European Parliament.
+#' 
+#' @docType package
+NULL
+
+
+#' @title Grand Paris Metropole Communes SpatialPolygonsDataFrame
+#' @name com.spdf
+#' @description SpatialPolygonsDataFrame of the Grand Paris Metropole communes.\cr 
+#' @format 
+#' \describe{
+#' \item{DEPCOM}{Commune identifiers}
+#' }
+#' @source 
+#' Institut national de l’information géographique et forestière (IGN), GEOFLA® 
+#' 2015 v2.1 Communes France Métropolitaine: \cr
+#' \url{http://professionnels.ign.fr/geofla}\cr
+#' Atelier parisien d'urbanisme, Grand Paris communal composition (2015-12-17):\cr
+#' \url{http://www.apur.org/article/composition-12-territoires-metropole-grand-paris}
+#' @docType data
+#' @examples
+#' data(GrandParisMetropole)
+#' sp::plot(com.spdf)
+NULL
+
+
+#' @title Grand Paris Metropole EPTs SpatialPolygonsDataFrame
+#' @name ept.spdf
+#' @description SpatialPolygonsDataFrame of Grand Paris Metropole EPTs. 
+#' EPTs (Etablissements Publics Territoriaux) are groups of communes.\cr
+#' @format 
+#' \describe{
+#' \item{EPT}{EPT identifiers}
+#' \item{LIBEPT}{EPT names}
+#' }
+#' @source
+#' Atelier parisien d'urbanisme, Grand Paris communal composition (2015-12-17):\cr
+#' \url{http://www.apur.org/article/composition-12-territoires-metropole-grand-paris}
+#' @docType data
+#' @examples
+#' data(GrandParisMetropole)
+#' sp::plot(ept.spdf)
+NULL
+
+#' @title Grand Paris Metropole Communes Data
+#' @name com
+#' @description Data on the Grand Paris Metropole communes.
+#' @format A data frame with 150 rows and 10 variables:
+#' \describe{
+#' \item{DEPCOM}{Commune identifiers}
+#' \item{LIBCOM}{Commune names}
+#' \item{EPT}{EPT identifiers of the commune}
+#' \item{LIBEPT}{EPT names of the commune}
+#' \item{DEP}{Identifiers of the departement}
+#' \item{INC}{Amount of income tax reference (in euros)}
+#' \item{TH}{Number of tax households}
+#' }
+#' @source
+#' Direction générale des finances publiques, income tax 2014 (2013 incomes):\cr
+#' \url{http://www.impots.gouv.fr/portal/dgi/public/statistiques.impot?espId=-4&pageId=stat_donnees_detaillees&sfid=4503}\cr
+#' Atelier parisien d'urbanisme, Grand Paris communal composition (2015-12-17):\cr
+#' \url{http://www.apur.org/article/composition-12-territoires-metropole-grand-paris}
+#' @docType data
+#' @examples
+#' data(GrandParisMetropole)
+#' head(com)
+NULL
+
+
+#' @title Time Distance Matrix Between Communes
+#' @name cardist
+#' @description Travel time between Grand Paris Metropole communes' centroids by car, in minutes.\cr
+#' Row names and column names match the DEPCOM field in \link{com}.
+#' @source
+#' The matrix is computed using the osrm package (\url{https://cran.r-project.org/package=osrm}). \cr
+#' Data (c) OpenStreetMap contributors, ODbL 1.0. http://www.openstreetmap.org/copyright \cr
+#' Routes: OSRM. http://project-osrm.org/ \cr
+#' 
+#' @docType data
+#' @examples
+#' data(GrandParisMetropole)
+#' cardist[1:10,1:10]
+NULL
+
+
+
+
+
