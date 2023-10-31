163b4a487b89aed97324d7f9da8afe6f5bd95e0c
Timothée Giraud <timothee.giraud@cnrs.fr>
2023-10-31 10:40:02 +0000

    version 0.6.0

--- inst/doc/MTA.R
+++ inst/doc/MTA.R
@@ -1,14 +1,14 @@
-## ---- fig.width=7, fig.height = 4.7,echo=FALSE--------------------------------
+## ----fig.width=7, fig.height = 4.7,echo=FALSE---------------------------------
 library(png)
 library(grid)
 img <- readPNG("./img/figure_MTA1.png")
 grid.raster(img)
 
-## ----  fig.width=7, fig.height = 4.8,echo=FALSE-------------------------------
+## ----fig.width=7, fig.height = 4.8,echo=FALSE---------------------------------
 img <- readPNG("./img/figure_MTA2.png")
 grid.raster(img)
 
-## ----  fig.width=7, fig.height = 6,echo=FALSE---------------------------------
+## ----fig.width=7, fig.height = 6,echo=FALSE-----------------------------------
 img <- readPNG("./img/figure_MTA3.png")
 grid.raster(img)
 
